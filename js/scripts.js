36c11a6b1c4e0b20a1bf0d76d74898cbc67a7081
Nhat Hoang and Sheena Nickerson <nhat@>
2016-08-17 10:28:52 -0700

    create first if paremeter statement to add ay to the end of a vowel.

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,14 @@
-$()
+$(document).ready(function(){
+  $(".form-group").submit(function(event){
+    var pigLInput = $("input#plInput").val();
+    var vowels = ["a", "e", "i", "o", "u", "A", "E", "I", "O", "U"];
+    var pigLOutput = ("");
+    for(i=0; i<=vowels.length; i+=1)
+      if (pigLInput === vowels[i]){
+        pigLOutput = pigLInput + "ay"
+        alert(pigLOutput);
+      };
+
+    event.preventDefault();
+  });
+});
