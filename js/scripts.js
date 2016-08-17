1ee4d7bebbf16924e0086672633e2d110e8f5e05
Nhat Hoang and Sheena Nickerson <nhat@>
2016-08-17 14:51:24 -0700

    add an if else statement to add consonants to end of word, and break to end the loop when it hits a vowel

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,30 @@
+
+
 $(document).ready(function(){
   $(".form-group").submit(function(event){
     var pigLInput = $("input#plInput").val();
-    var vowels = ["a", "e", "i", "o", "u", "A", "E", "I", "O", "U"];
+    // var vowels = ["a", "e", "i", "o", "u", "A", "E", "I", "O", "U"];
     var pigLOutput = ("");
-    for(i=0; i<=vowels.length; i+=1)
-      if (pigLInput === vowels[i]){
-        pigLOutput = pigLInput + "ay"
-        alert(pigLOutput);
+    var array = pigLInput.split("");
+    var array2 = [""];
+    pigLOutput = array;
+
+
+    for(j=0; j<=array.length; j+=1)
+      if (array[j] === "a" || array[j] === "e" || array[j] === "i" || array[j] === "o" || array[j] === "u") {
+        var newArray = array.push("ay");
+        console.log(array);
+        alert(array);
+        break;
+
+      }else {
+        alert("else");
+        array.push(array[j]);
+        console.log(array);
       };
 
-    event.preventDefault();
+      $(".pigLatinOutput").text(pigLOutput);
+
+  event.preventDefault();
   });
 });
