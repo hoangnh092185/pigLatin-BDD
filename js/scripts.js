c56de4d79d5503ea886f7129e3eb3759b3b199cd
Nhat Hoang and Sheena Nickerson <nhat@>
2016-08-17 15:28:58 -0700

    convert single word to pig latin.

--- js/scripts.js
+++ js/scripts.js
@@ -2,28 +2,35 @@
 
 $(document).ready(function(){
   $(".form-group").submit(function(event){
-    var pigLInput = $("input#plInput").val();
+    var pigLInput = $("input#plInput").val().toLowerCase();
     // var vowels = ["a", "e", "i", "o", "u", "A", "E", "I", "O", "U"];
     var pigLOutput = ("");
     var array = pigLInput.split("");
-    var array2 = [""];
-    pigLOutput = array;
-
+    var arrayCopy = array.slice();
 
     for(j=0; j<=array.length; j+=1)
       if (array[j] === "a" || array[j] === "e" || array[j] === "i" || array[j] === "o" || array[j] === "u") {
-        var newArray = array.push("ay");
+        arrayCopy.push("ay");
         console.log(array);
         alert(array);
         break;
 
-      }else {
+      }else if(array[j]=== "q"){
+        arrayCopy.push("qu")
+        arrayCopy.shift();
+        arrayCopy.shift();
+      }else if (array[j]=== "y") {
+        arrayCopy.push(array[j]);
+        arrayCopy.shift();
+      }else{
         alert("else");
-        array.push(array[j]);
-        console.log(array);
+        arrayCopy.push(array[j]);
+        console.log(arrayCopy);
+        arrayCopy.shift();
       };
 
-      $(".pigLatinOutput").text(pigLOutput);
+    pigLOutput = arrayCopy.join("");
+    $(".pigLatinOutput").text(pigLOutput);
 
   event.preventDefault();
   });
