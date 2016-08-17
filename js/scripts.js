b44d085125aefcbdef05ef5dcdcbd5bdf0b22a2e
Nhat Hoang and Sheena Nickerson <nhat@>
2016-08-17 16:54:31 -0700

    Add function with multiple wordsto work. functions is incomplete.

--- js/scripts.js
+++ js/scripts.js
@@ -5,30 +5,34 @@ $(document).ready(function(){
     var pigLInput = $("input#plInput").val().toLowerCase();
     // var vowels = ["a", "e", "i", "o", "u", "A", "E", "I", "O", "U"];
     var pigLOutput = ("");
-    var array = pigLInput.split("");
-    var arrayCopy = array.slice();
+    var array = pigLInput.split(" ");
 
-    for(j=0; j<=array.length; j+=1)
-      if (array[j] === "a" || array[j] === "e" || array[j] === "i" || array[j] === "o" || array[j] === "u") {
-        arrayCopy.push("ay");
-        console.log(array);
-        alert(array);
-        break;
+    for(i=0; i<=array.length; i+=1)
+      var stringArray = array[i].toString();
+      var splitArray = stringArray.split("");
+      var arrayCopy= splitArray.slice()
+      console.log(arrayCopy);
 
-      }else if(array[j]=== "q"){
-        arrayCopy.push("qu")
-        arrayCopy.shift();
-        arrayCopy.shift();
-      }else if (array[j]=== "y") {
-        arrayCopy.push(array[j]);
-        arrayCopy.shift();
-      }else{
-        alert("else");
-        arrayCopy.push(array[j]);
-        console.log(arrayCopy);
-        arrayCopy.shift();
-      };
-
+        for(j=0; j<=splitArray.length; j+=1)
+            if (splitArray[j] === "a" || splitArray[j] === "e" || splitArray[j] === "i" || splitArray[j] === "o" || splitArray[j] === "u") {
+            arrayCopy.push("ay");
+            console.log(splitArray);
+            alert(splitArray);
+            break;
+          }else if(splitArray[j]=== "q"){
+              arrayCopy.push("qu")
+            arrayCopy.shift();
+            arrayCopy.shift();
+            }else if (splitArray[j]=== "y") {
+            arrayCopy.push(splitArray[j]);
+            arrayCopy.shift();
+            }else{
+              alert("else");
+            arrayCopy.push(splitArray[j]);
+            console.log(arrayCopy);
+            arrayCopy.shift();
+          };
+          pigLOuput +=arrayCopy;
     pigLOutput = arrayCopy.join("");
     $(".pigLatinOutput").text(pigLOutput);
 
